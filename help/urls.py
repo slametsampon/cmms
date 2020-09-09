790edcba925d6090f70803f33a3bf5cc86ea5aa7
sampon <slamet@pon.co.id>
2020-09-09 18:27:42 +0700

    Completed skeleton

--- help/urls.py
+++ help/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 app_name = 'help'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.cmms, name='cmms'),
+    path('workOrder/', views.workOrder, name='workOrder-Help'),
+    path('PmPdm/', views.PmPdm, name='PmPdm-Help'),
+    path('Equipment/', views.Equipment, name='Equipment-Help'),
+    path('Utility/', views.Utility, name='Utility-Help'),
+    path('Reporting/', views.Reporting, name='Reporting-Help'),
 ]
--- help/views.py
+++ help/views.py
@@ -1,8 +1,39 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
+def cmms(request):
     """View function for home page of site."""
 
     # Render the HTML template index.html with the data in the context variable
-    return render(request, 'indexHelp.html')
+    return render(request, 'cmms.html')
+
+def workOrder(request):
+    """View function for home page of site."""
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'workOrder.html')
+
+def PmPdm(request):
+    """View function for home page of site."""
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'PmPdm.html')
+
+def Equipment(request):
+    """View function for home page of site."""
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'equipment.html')
+
+def Reporting(request):
+    """View function for home page of site."""
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'reporting.html')
+
+def Utility(request):
+    """View function for home page of site."""
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'utility.html')
+
