d6ab6d685587bfa509f63d095092648286ba2c4d
sampon <slamet@pon.co.id>
2020-09-10 12:41:05 +0700

    update profile

--- workOrder/admin.py
+++ workOrder/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Section, Department, Work_order, Work_order_completion, Work_order_journal
+from .models import Section, Department, Work_order, Work_order_completion, Work_order_journal, Profile
 
 # Register your models here.
 # Register the Admin classes for Department using the decorator
@@ -14,6 +14,11 @@ class SectionAdmin(admin.ModelAdmin):
     
 # Register the Admin classes for Section using the decorator
 @admin.register(Work_order_journal)
+class Work_order_journalAdmin(admin.ModelAdmin):
+    pass
+    
+# Register the Admin classes for Section using the decorator
+@admin.register(Profile)
 class Work_order_journalAdmin(admin.ModelAdmin):
     pass
     
