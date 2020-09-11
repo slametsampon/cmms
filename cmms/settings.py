b969473689418a80ed634d6f0ab5192e0f30adec
sampon <slamet@pon.co.id>
2020-09-11 20:20:11 +0700

    images not working

--- cmms/settings.py
+++ cmms/settings.py
@@ -151,6 +151,10 @@ STATIC_URL = '/static/'
 # The absolute path to the directory where collectstatic will collect static files for deployment.
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
 LOGIN_REDIRECT_URL = '/'
 
